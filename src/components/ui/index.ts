022794dac05e2eb5093f3dc450595be5de25a991
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-25 22:04:18 +0900

    🚀 プロジェクトの開始

---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+// biome-ignore lint/correctness/useImportExtensions: using barrel file
+// biome-ignore lint/performance/noReExportAll: using barrel file
+// biome-ignore lint/performance/noBarrelFile: using barrel file
+export * from './primitive'
