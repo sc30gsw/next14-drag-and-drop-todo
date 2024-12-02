d3554233db3115b001a00806e34ac2ad1d7de546
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-02 21:06:14 +0900

    🚑 各種不要ライブラリを削除

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import { QueryProvider } from '@/components/query-provider'
-import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import type { ReactNode } from 'react'
 import { RouterProvider } from 'react-aria-components'
-import { v4 as uuidv4 } from 'uuid'
 
 declare module 'react-aria-components' {
   interface RouterConfig {
@@ -18,7 +16,6 @@ declare module 'react-aria-components' {
 
 export function Providers({ children }: { children: ReactNode }) {
   const router = useRouter()
-  Cookies.set('nothing', uuidv4())
 
   return (
     <RouterProvider navigate={router.push}>
